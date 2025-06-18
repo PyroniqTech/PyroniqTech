ee7226e9cc53b01f6ca0be8e3551077776108cf5
Muhammad Umair Mustafa <umairrajput04@gmail.com>
2025-06-18 13:03:11 +0500

    Update settings.py

--- PyroniqTech/settings.py
+++ PyroniqTech/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -68,7 +69,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'main' / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'main' / 'static'
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
