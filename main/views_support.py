46aa1ed62cec45f32c10466696be19f2259e0d20
Muhammad Umair Mustafa <umairrajput04@gmail.com>
2025-06-19 19:04:55 +0500

    Update views_support.py

--- main/views_support.py
+++ main/views_support.py
@@ -3,26 +3,26 @@ from django.shortcuts import render
 def support_home(request):
     return render(request, 'support/support_home.html')
 
-def general_inquiries(request):
-    return render(request, 'support/general_inquiries.html')
+def general(request):
+    return render(request, 'support/general.html')
 
-def ai_bot_assistance(request):
-    return render(request, 'support/ai_bot_assistance.html')
+def ai_bot_help(request):
+    return render(request, 'support/ai_bot_help.html')
 
-def live_chat_support(request):
-    return render(request, 'support/live_chat_support.html')
+def live_chat(request):
+    return render(request, 'support/live_chat.html')
 
-def order_status(request):
-    return render(request, 'support/order_status.html')
+def project_status(request):
+    return render(request, 'support/project_status.html')
 
 def automation_help(request):
     return render(request, 'support/automation_help.html')
 
-def technical_support(request):
-    return render(request, 'support/technical_support.html')
+def technical(request):
+    return render(request, 'support/technical.html')
 
-def payments_billing(request):
-    return render(request, 'support/payments_billing.html')
+def payment_issues(request):
+    return render(request, 'support/payment_issues.html')
 
 def bot_installation(request):
     return render(request, 'support/bot_installation.html')
