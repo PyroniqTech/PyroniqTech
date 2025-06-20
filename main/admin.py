50c2869c0a82f86d2b0c349d86c0bb1885a0e7ce
Muhammad Umair Mustafa <umairrajput04@gmail.com>
2025-06-20 08:21:27 +0500

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import SupportTicket, TicketReply
+
+@admin.register(SupportTicket)
+class SupportTicketAdmin(admin.ModelAdmin):
+    list_display = ('ticket_id', 'name', 'email', 'status', 'created_at')
+    search_fields = ('ticket_id', 'email', 'name')
+    list_filter = ('status',)
+
+@admin.register(TicketReply)
+class TicketReplyAdmin(admin.ModelAdmin):
+    list_display = ('ticket', 'by_admin', 'created_at')
+    search_fields = ('ticket__ticket_id',)
