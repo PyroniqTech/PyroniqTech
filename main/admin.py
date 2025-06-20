94456b661016f70e0b4a5f54b30764cecde90045
Muhammad Umair Mustafa <umairrajput04@gmail.com>
2025-06-20 09:03:47 +0500

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -6,6 +6,8 @@ class SupportTicketAdmin(admin.ModelAdmin):
     list_display = ('ticket_id', 'name', 'email', 'status', 'created_at')
     search_fields = ('ticket_id', 'email', 'name')
     list_filter = ('status',)
+    readonly_fields = ('ticket_id', 'created_at')  # ✅ Safe fields
+
 
 @admin.register(TicketReply)
 class TicketReplyAdmin(admin.ModelAdmin):
